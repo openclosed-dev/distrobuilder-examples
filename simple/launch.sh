2ec386d257ccfaeb600467b7d0a6454ebe6f6ec6
leadpony <leadpony@openclosed.dev>
2024-05-19 22:39:26 +0900

    Keep other images

--- simple/launch.sh
+++ simple/launch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-lxc list --format csv -c n | xargs -I {} lxc delete -f {}
-lxc image list --format csv -c l | xargs -I {} lxc image delete {}
+imageAlias=simple
+containerName=test
 
-lxc image import incus.tar.gz rootfs.squashfs --alias simple
-lxc launch simple test
-lxc config device add test rdp proxy listen=tcp:0.0.0.0:3389 connect=tcp:0.0.0.0:3389 bind=host
+lxc list --format csv -c n name=$containerName | xargs -I {} lxc delete -f {}
+lxc image list --format csv -c l $imageAlias | xargs -I {} lxc image delete {}
+
+lxc image import incus.tar.gz rootfs.squashfs --alias $imageAlias
+lxc launch $imageAlias $containerName
+lxc config device add $containerName rdp proxy listen=tcp:0.0.0.0:3389 connect=tcp:0.0.0.0:3389 bind=host
