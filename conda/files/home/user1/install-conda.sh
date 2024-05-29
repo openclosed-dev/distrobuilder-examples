66e9140e501298e6a77aa0b275f781329a4d61bf
leadpony <leadpony@openclosed.dev>
2024-05-29 21:16:57 +0900

    Cache downloaded miniconda installer

--- conda/files/home/user1/install-conda.sh
+++ conda/files/home/user1/install-conda.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -f ~/miniconda3/miniconda.sh
+bash ~/miniconda.sh -b -u -p ~/miniconda3
 
 cd ~/miniconda3/bin
-
 ./conda init bash
-./conda update -n base -y conda
+
+./conda update -n base -c defaults -y conda
 ./conda install -c conda-forge -y jupyterlab
